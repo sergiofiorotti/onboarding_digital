d8214d0c1d5c29e5ee60322f39e565c3e550a898
tiagofoks <tiagofoks@yahoo.com.br>
2017-06-03 21:00:21 -0300

    Include Allow methods, allow heathers, and bodyparser

--- server.js
+++ server.js
@@ -8,9 +8,28 @@ const processRouter = require('./modules/process/route')
 const app = express()
 app.use(cors())
 
+app.use(allowCors);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+	
+	extended: true
+}));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+var allowCors = function(req, res, next){
+	
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Credentials', 'true');
+
+	next();
+}
 
 app.use(clientRouter.router)
 app.use(imageRouter.router)
@@ -18,4 +37,4 @@ app.use(processRouter.router)
 
 app.listen(3000, function () {
   console.log('UX Match! api listening on port 3000!')
-})
+});
